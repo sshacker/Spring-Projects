4848bc638ca09ea17362a7539f38d177afe7d6d3
shivamsonkar260@gmail.com <shivamsonkar260@gmail.com>
2022-02-11 13:51:51 +0530

    First Maven Project Auto Generated Default Hello World

---
+++ maven/src/main/java/com/sshacker/maven/demo/App.java
@@ -0,0 +1,13 @@
+package com.sshacker.maven.demo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "First Maven Project Hello World!" );
+    }
+}
