cfc7f69b630d4261adc8c73145448a4ebe4258dd
shivamsonkar260@gmail.com <shivamsonkar260@gmail.com>
2022-02-12 19:34:50 +0530

    conflict resolution using @Qualifier(snapdragon) Annotation between Snapdragon and Mediatek

--- springannotation/src/main/java/com/sshacker/springannotation/Samsung.java
+++ springannotation/src/main/java/com/sshacker/springannotation/Samsung.java
@@ -1,11 +1,14 @@
 package com.sshacker.springannotation;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 @Component
 public class Samsung {
+	
 	@Autowired
+	@Qualifier("snapdragon")
 	MobileProcessor cpu;
 	
 	public void config() {
