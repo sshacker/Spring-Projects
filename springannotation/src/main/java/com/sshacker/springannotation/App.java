ed4b5904dbb13c3e69e2de1232df965af6a4fe04
shivamsonkar260@gmail.com <shivamsonkar260@gmail.com>
2022-02-12 16:45:21 +0530

    New Maven Project springannotation creating simple naive methode initial

---
+++ springannotation/src/main/java/com/sshacker/springannotation/App.java
@@ -0,0 +1,10 @@
+package com.sshacker.springannotation;
+
+public class App 
+{
+    public static void main( String[] args )
+    {
+        Sumsung s7 = new Sumsung();
+        s7.config();
+    }
+}
---
+++ springannotation/src/main/java/com/sshacker/springannotation/Sumsung.java
@@ -0,0 +1,7 @@
+package com.sshacker.springannotation;
+
+public class Sumsung {
+	public void config() {
+		System.out.println("Octa core, Ram 4GB, Camera 48MP");
+	}
+}
