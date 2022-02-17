d889c6bfe2c98bbfc5e3411ab1316807d35f98a1
shivamsonkar260@gmail.com <shivamsonkar260@gmail.com>
2022-02-17 11:05:54 +0530

    using XmlBeanFactory class - deprecated

--- hellospring/src/com/sshacker/tutorialpoint/MainApp.java
+++ hellospring/src/com/sshacker/tutorialpoint/MainApp.java
@@ -1,12 +1,21 @@
 package com.sshacker.tutorialpoint;
 
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
+// import org.springframework.context.ApplicationContext;
+// import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
 
 public class MainApp {
+
+    @SuppressWarnings("deprecation")
     public static void main(String[] args) {
-    	ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
-    	HelloWorld obj = (HelloWorld) context.getBean("helloworld");
-    	obj.getMessage();
+        // ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+        // HelloWorld obj = (HelloWorld) context.getBean("helloworld");
+
+        // using XmlBeanfactory - deprecated
+        XmlBeanFactory factory = new XmlBeanFactory(new ClassPathResource("Beans.xml"));
+        HelloWorld obj = (HelloWorld) factory.getBean("helloworld");
+        obj.getMessage();
     }
 }
