a176823497163376244b32a73d2212aad13faba2
shivamsonkar260@gmail.com <shivamsonkar260@gmail.com>
2022-02-17 07:31:55 +0530

    spring tutorialspoint first HelloWorld java project all spring configuration manually and working fine

---
+++ hellospring/src/com/sshacker/tutorialpoint/HelloWorld.java
@@ -0,0 +1,14 @@
+package com.sshacker.tutorialpoint;
+
+public class HelloWorld {
+    private String message;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public void getMessage() {
+        System.out.println("Your message : " + message);
+    }
+
+}
---
+++ hellospring/src/com/sshacker/tutorialpoint/MainApp.java
@@ -0,0 +1,12 @@
+package com.sshacker.tutorialpoint;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+    public static void main(String[] args) {
+    	ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
+    	HelloWorld obj = (HelloWorld) context.getBean("helloworld");
+    	obj.getMessage();
+    }
+}
