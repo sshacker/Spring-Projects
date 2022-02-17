67ce6e070f4cc37e748bb45d1016ffed0679fb2a
shivamsonkar260@gmail.com <shivamsonkar260@gmail.com>
2022-02-17 13:45:42 +0530

    using FileSystemXmlApplicationContext by providing full path for Bean configuration xml file

--- hellospring/src/com/sshacker/tutorialpoint/MainApp.java
+++ hellospring/src/com/sshacker/tutorialpoint/MainApp.java
@@ -1,22 +1,21 @@
 package com.sshacker.tutorialpoint;
 
-// import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 // import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import org.springframework.beans.factory.xml.XmlBeanFactory;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.beans.factory.BeanFactory;
 
 public class MainApp {
 
-    @SuppressWarnings("deprecation")
     public static void main(String[] args) {
+    	// // using ClassPathXmlApplicationContext - providing file name at CLASSPATH
         // ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
         // HelloWorld obj = (HelloWorld) context.getBean("helloworld");
-
-        // using XmlBeanfactory - deprecated
-        BeanFactory factory = new XmlBeanFactory(new ClassPathResource("Beans.xml"));
-        HelloWorld obj = (HelloWorld) factory.getBean("helloworld");
+    	
+    	//using FileSystemXmlApplicationContext - providing full path
+        ApplicationContext context = new FileSystemXmlApplicationContext(
+                "C:/Users/shiva/OneDrive/Desktop/Spring-Projects/hellospring/src/Beans.xml");
+        HelloWorld obj = (HelloWorld) context.getBean("helloworld");
         obj.getMessage();
+        
     }
 }
