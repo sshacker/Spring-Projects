d067ca5611a6d22813b8de29062d11f71edc5183
shivamsonkar260@gmail.com <shivamsonkar260@gmail.com>
2022-02-17 18:22:09 +0530

    example of singleton scope 2

--- hellospring/src/com/sshacker/tutorialpoint/MainApp.java
+++ hellospring/src/com/sshacker/tutorialpoint/MainApp.java
@@ -6,11 +6,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MainApp {
 
     public static void main(String[] args) {
-    	
-    	// example of singleton scope
+
+        // example of singleton scope
         ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 
         HelloWorld obj1 = (HelloWorld) context.getBean("helloworld");
+        obj1.setMessage("Hello World !!!!!");
         obj1.getMessage();
 
         HelloWorld obj2 = (HelloWorld) context.getBean("helloworld");
