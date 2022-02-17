d228980fc2f9ce9205ae1895507b3fec69201cc5
shivamsonkar260@gmail.com <shivamsonkar260@gmail.com>
2022-02-17 21:54:40 +0530

    Bean Defination Templete

--- hellospring2/src/com/sshacker/tutorialpoint/MainApp.java
+++ hellospring2/src/com/sshacker/tutorialpoint/MainApp.java
@@ -7,10 +7,6 @@ public class MainApp {
     public static void main(String[] args) {
         ApplicationContext context = new ClassPathXmlApplicationContext("Beans.xml");
 
-        HelloWorld world = (HelloWorld) context.getBean("world");
-        world.getMessage1();
-        world.getMessage2();
-
         HelloIndia india = (HelloIndia) context.getBean("india");
         india.getMessage1();
         india.getMessage2();
