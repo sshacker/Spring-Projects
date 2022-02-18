17fb28235c75b57749e634b0d3a4b2386b874596
shivamsonkar260@gmail.com <shivamsonkar260@gmail.com>
2022-02-19 05:25:28 +0530

    Injecting Inner Beans\

--- importantspring/src/com/sshacker/tutorialpoint/MainApp.java
+++ importantspring/src/com/sshacker/tutorialpoint/MainApp.java
@@ -6,9 +6,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MainApp {
 
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("DependencyInjectionBeans.xml");
-		John john = (John) context.getBean("john");
-		john.johnDetails();
-
+		ApplicationContext context = new ClassPathXmlApplicationContext("InjectingInnerBeans.xml");
+		TextEditor te = (TextEditor) context.getBean("texteditor");
+		te.textEditorSpellingChecking();
 	}
 }
