ee5abbcea2d6dbee0823b53b18dcb485c2222ccd
shivamsonkar260@gmail.com <shivamsonkar260@gmail.com>
2022-02-20 10:21:20 +0530

    to enable Annotation based configuration in xml file

--- importantspring/src/com/sshacker/tutorialpoint/MainApp.java
+++ importantspring/src/com/sshacker/tutorialpoint/MainApp.java
@@ -6,8 +6,6 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MainApp {
 
 	public static void main(String[] args) {
-		ApplicationContext context = new ClassPathXmlApplicationContext("AutowireConstructor.xml");
-		TextEditor te = (TextEditor) context.getBean("texteditor");
-		te.textEditorSpellingChecking();
+		ApplicationContext context = new ClassPathXmlApplicationContext("AnnotationBase.xml");
 	}
 }
