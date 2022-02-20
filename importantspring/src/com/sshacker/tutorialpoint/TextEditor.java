ff28fef9b610c39763a6ee1223d002204dd3fcd0
shivamsonkar260@gmail.com <shivamsonkar260@gmail.com>
2022-02-20 15:31:49 +0530

    @Autowired on Constructors

--- importantspring/src/com/sshacker/tutorialpoint/TextEditor.java
+++ importantspring/src/com/sshacker/tutorialpoint/TextEditor.java
@@ -4,8 +4,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 public class TextEditor {
 
-	@Autowired
 	private SpellChecker spellChecker;
+
+	@Autowired
+	public TextEditor(SpellChecker spellChecker) {
+		System.out.println("@Autowired constructor");
+		this.spellChecker = spellChecker;
+	}
 
 	public void textEditorSpellingChecking() {
 		spellChecker.checkingSpelling();
