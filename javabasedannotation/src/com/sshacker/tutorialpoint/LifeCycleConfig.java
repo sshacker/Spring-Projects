99d2a0ffa04a9ce037a685991bea8bbe665c57af
shivamsonkar260@gmail.com <shivamsonkar260@gmail.com>
2022-02-20 20:06:46 +0530

    @Scope prototype and sigletone Java Based Configuration

--- javabasedannotation/src/com/sshacker/tutorialpoint/LifeCycleConfig.java
+++ javabasedannotation/src/com/sshacker/tutorialpoint/LifeCycleConfig.java
@@ -2,10 +2,12 @@ package com.sshacker.tutorialpoint;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 public class LifeCycleConfig {
     @Bean(initMethod = "init", destroyMethod = "cleanup")
+    @Scope("prototype")
     public LifeCycle lifeCycle() {
         return new LifeCycle();
     }
