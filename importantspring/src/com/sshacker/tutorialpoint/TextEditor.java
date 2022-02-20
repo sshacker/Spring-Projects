bbe22409313cb5ca5b149bed18dce0d044d59814
shivamsonkar260@gmail.com <shivamsonkar260@gmail.com>
2022-02-20 15:28:20 +0530

    @Autowired on Properties

--- importantspring/src/com/sshacker/tutorialpoint/TextEditor.java
+++ importantspring/src/com/sshacker/tutorialpoint/TextEditor.java
@@ -3,16 +3,9 @@ package com.sshacker.tutorialpoint;
 import org.springframework.beans.factory.annotation.Autowired;
 
 public class TextEditor {
-	private SpellChecker spellChecker;
-
-	public SpellChecker getSpellChecker() {
-		return spellChecker;
-	}
 
 	@Autowired
-	public void setSpellChecker(SpellChecker spellChecker) {
-		this.spellChecker = spellChecker;
-	}
+	private SpellChecker spellChecker;
 
 	public void textEditorSpellingChecking() {
 		spellChecker.checkingSpelling();
