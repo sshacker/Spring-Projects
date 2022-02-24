567514fc488c28b6af23d249541ca3aa00c8f9b2
shivamsonkar260@gmail.com <shivamsonkar260@gmail.com>
2022-02-24 09:40:40 +0530

    XML Schema Based AOP with Spring - working

--- aop/src/com/sshacker/tutorialpoint/aop/MainApp.java
+++ aop/src/com/sshacker/tutorialpoint/aop/MainApp.java
@@ -4,13 +4,12 @@ import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class MainApp {
-
 	public static void main(String[] args) {
 		ApplicationContext context = new ClassPathXmlApplicationContext("Aop.xml");
-		Student s = (Student) context.getBean("student");
-		System.out.println(s.getName());
-		System.out.println(s.getAge());
-		s.printThrowException();
-	}
 
+		Student student = (Student) context.getBean("student");
+		student.getName();
+		student.getAge();
+		student.printThrowException();
+	}
 }
--- aop/src/com/sshacker/tutorialpoint/aop/Student.java
+++ aop/src/com/sshacker/tutorialpoint/aop/Student.java
@@ -4,25 +4,26 @@ public class Student {
     private Integer age;
     private String name;
 
-    public Integer getAge() {
-        return age;
-    }
-
     public void setAge(Integer age) {
         this.age = age;
     }
 
-    public String getName() {
-        return name;
+    public Integer getAge() {
+        System.out.println("Age : " + age);
+        return age;
     }
 
     public void setName(String name) {
         this.name = name;
     }
 
-    public void printThrowException() {
-        System.out.println("Throw exception raised");
-        throw new IllegalArgumentException();
+    public String getName() {
+        System.out.println("Name : " + name);
+        return name;
     }
 
+    public void printThrowException() {
+        System.out.println("Exception raised");
+        throw new IllegalArgumentException();
+    }
 }
