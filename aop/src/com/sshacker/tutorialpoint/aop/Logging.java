ba14385ffd9e0d97e957a8c054f7c5c47b0768dc
shivamsonkar260@gmail.com <shivamsonkar260@gmail.com>
2022-02-24 18:27:23 +0530

    @AspectJ Based AOP with Spring

--- aop/src/com/sshacker/tutorialpoint/aop/Logging.java
+++ aop/src/com/sshacker/tutorialpoint/aop/Logging.java
@@ -1,10 +1,22 @@
 package com.sshacker.tutorialpoint.aop;
 
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+
 public class Logging {
+
+	@Pointcut("execution(* com.sshacker.sshacker.tutorialpoint.*.*(..))")
+	private void selectedAll() {
+	}
+
 	/**
 	 * This is the method which I would like to execute
 	 * before a selected method execution.
 	 */
+	@Before("selectAll")
 	public void beforeAdvice() {
 		System.out.println("Going to setup student profile.");
 	}
@@ -13,6 +25,7 @@ public class Logging {
 	 * This is the method which I would like to execute
 	 * after a selected method execution.
 	 */
+	@After("selectAll")
 	public void afterAdvice() {
 		System.out.println("Student profile has been setup.");
 	}
@@ -21,6 +34,7 @@ public class Logging {
 	 * This is the method which I would like to execute
 	 * when any method returns.
 	 */
+	@AfterReturning(pointcut = "selectAll", returning = "retVal")
 	public void afterReturningAdvice(Object retVal) {
 		System.out.println("Returning:" + retVal.toString());
 	}
@@ -29,6 +43,7 @@ public class Logging {
 	 * This is the method which I would like to execute
 	 * if there is an exception raised.
 	 */
+	@AfterThrowing(pointcut = "selectAll", throwing = "ex")
 	public void afterThrowingAdvice(IllegalArgumentException ex) {
 		System.out.println("There has been an exception: " + ex.toString());
 	}
